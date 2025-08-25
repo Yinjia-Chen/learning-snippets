96de68964d6847c1d71d822fba67200846f71726
陈尹嘉 <chenyj@dunhefund.com>
2025-08-25 10:37:53 +0800

    docs: 修正注释

--- regex/demos/1.两种写法.js
+++ regex/demos/1.两种写法.js
@@ -9,7 +9,7 @@ const regexp2 = new RegExp(123) // js 会自动 ToString
 
 // 这两种写法都是匹配连续字符字串 即是否存在子串 "abc" "123"
 
-console.log(Object.prototype.toString.call(regexp1)); // [object regexp1]
+console.log(Object.prototype.toString.call(regexp1)); // [object RegExp]
 console.log(typeof regexp1); // object
 console.log(regexp1); // /abc/
 console.log(regexp2); // /123/
